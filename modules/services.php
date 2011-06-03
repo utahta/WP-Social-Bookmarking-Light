6aab58b6a6d633c6b24d37ada86619ab1409671d
utahvich <utahvich@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-03 12:37:33 +0000

    Add Google +1
    git-svn-id: http://plugins.svn.wordpress.org/wp-social-bookmarking-light/trunk@392706 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/services.php
+++ modules/services.php
@@ -308,7 +308,15 @@ class WpSocialBookmarkingLight
         $icon = WP_SOCIAL_BOOKMARKING_LIGHT_IMAGES_URL."/google-buzz.png";
         return $this->link( $url, $alt, $icon, 16, 16 );
     }
-    
+    /**
+     * @brief Google Buzz
+     */
+    function google_plus_one()
+    {
+        $raw = '<g:plusone size="medium"></g:plusone>';
+        return $this->link_raw($raw);
+    }
+
     /**
      * @brief Delicious
      */
