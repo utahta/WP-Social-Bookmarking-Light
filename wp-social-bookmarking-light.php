c880a9aea82ca6f802327a198424226fc94c6965
utahta <labs.ninxit@gmail.com>
2017-01-06 00:28:04 +0900

    Update v1.8.8

--- wp-social-bookmarking-light.php
+++ wp-social-bookmarking-light.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/utahta/WP-Social-Bookmarking-Light
 Description: This plugin inserts social share links at the top or bottom of each post.
 Author: utahta
 Author URI: https://github.com/utahta/WP-Social-Bookmarking-Light
-Version: 1.8.7
+Version: 1.8.8
 */
 /*
 Copyright 2010 utahta (email : labs.ninxit@gmail.com)
