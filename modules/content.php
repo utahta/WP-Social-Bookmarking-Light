d294fd77d2576dacc507d2e860ecb422480f6ff7
utahta <labs.ninxit@gmail.com>
2017-03-01 21:53:35 +0900

    Apply PSR-2 code style and refactor

--- modules/content.php
+++ modules/content.php
@@ -25,23 +25,28 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * @param string $title
  * @return string
  */
-function wp_social_bookmarking_light_output( $services, $link, $title )
+function wp_social_bookmarking_light_output($services, $link, $title)
 {
-    $wp = new WpSocialBookmarkingLight( $link, $title, get_bloginfo('name') );
+    $wp = new WpSocialBookmarkingLight($link, $title, get_bloginfo('name'));
     $class_methods = wp_social_bookmarking_light_get_class_methods();
     $out = '';
-    foreach( explode(",", $services) as $service ){
+    foreach (explode(",", $services) as $service) {
         $service = trim($service);
-        if($service != ''){
-            if(in_array($service, $class_methods)){
-                $out .= '<div class="wsbl_'.$service.'">'.call_user_func( array( $wp, $service ) ).'</div>'; // WpSocialBookmarkingLight method
-            }
-            else{
-                $out .= "<div>[`$service` not found]</div>";
-            }
+        if ($service === '') {
+            continue;
+        }
+
+        if (in_array($service, $class_methods)) {
+            $out .= '<div class="wsbl_' . $service . '">' . call_user_func(array(
+                    $wp,
+                    $service
+                )) . '</div>'; // WpSocialBookmarkingLight method
+        } else {
+            $out .= "<div>[`$service` not found]</div>";
         }
     }
-    if( $out == '' ){
+
+    if ($out == '') {
         return $out;
     }
     return "<div class='wp_social_bookmarking_light'>{$out}</div><br class='wp_social_bookmarking_light_clear' />";
@@ -54,13 +59,13 @@ function wp_social_bookmarking_light_output( $services, $link, $title )
  * @param string $link
  * @param string $title
  */
-function wp_social_bookmarking_light_output_e( $services=null, $link=null, $title=null )
+function wp_social_bookmarking_light_output_e($services = null, $link = null, $title = null)
 {
-    if($services == null){
+    if ($services == null) {
         $options = wp_social_bookmarking_light_options();
         $services = $options['services'];
     }
-    echo wp_social_bookmarking_light_output( $services, $link, $title );
+    echo wp_social_bookmarking_light_output($services, $link, $title);
 }
 
 /**
@@ -68,52 +73,55 @@ function wp_social_bookmarking_light_output_e( $services=null, $link=null, $titl
  */
 function wp_social_bookmarking_light_wp_head()
 {
-?>
-<!-- BEGIN: WP Social Bookmarking Light -->
-<?php
-    // load options
+    $out = "<!-- BEGIN: WP Social Bookmarking Light -->\n";
+
+    // Load options
     $options = wp_social_bookmarking_light_options();
     $services = explode(",", $options['services']);
 
     // mixi-check-robots
-    if(in_array('mixi', $services)){
-?>
-<meta name="mixi-check-robots" content="<?php echo $options['mixi']['check_robots'] ?>" />
-<?php
+    if (in_array('mixi', $services)) {
+        $out .= '<meta name="mixi-check-robots" content="' . $options['mixi']['check_robots'] . '"/>' . "\n";
     }
 
-    // load javascript
     // tumblr
-    if(in_array('tumblr', $services)){
-        ?><script type="text/javascript" src="http://platform.tumblr.com/v1/share.js"></script><?php
+    if (in_array('tumblr', $services)) {
+        $out .= '<script type="text/javascript" src="http://platform.tumblr.com/v1/share.js"></script>';
     }
+
     // facebook
-    if(in_array('facebook_like', $services)  ||
-       in_array('facebook_share', $services) ||
-       in_array('facebook_send', $services)){
+    if (in_array('facebook_like', $services) ||
+        in_array('facebook_share', $services) ||
+        in_array('facebook_send', $services)
+    ) {
         $version = $options['facebook']['version'];
-        if($version == "html5" || $version == "xfbml"){
+        if ($version == "html5" || $version == "xfbml") {
             $locale = $options['facebook']['locale'];
             $locale = ($locale == '' ? 'en_US' : $locale);
-?>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/<?php echo $locale ?>/sdk.js#xfbml=1&version=v2.7";
-  fjs.parentNode.insertBefore(js, fjs);
+            $out .= <<<HTML
+<script>(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/$locale/sdk.js#xfbml=1&version=v2.7";
+    fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<?php
+
+HTML;
         }
     }
 
     // css
-?>
+    $out .= <<<HTML
 <style type="text/css">
-<?php echo $options['styles'] ?>
+    ${options['styles']}
 </style>
-<!-- END: WP Social Bookmarking Light -->
-<?php
+
+HTML;
+
+    $out .= "<!-- END: WP Social Bookmarking Light -->\n";
+    echo $out;
 }
 
 /**
@@ -123,7 +131,7 @@ function wp_social_bookmarking_light_wp_head()
  */
 function wp_social_bookmarking_light_is_enabled()
 {
-    if (is_feed() || is_404() || is_robots() || (function_exists( 'is_ktai' ) && is_ktai())) {
+    if (is_feed() || is_404() || is_robots() || (function_exists('is_ktai') && is_ktai())) {
         return false;
     }
 
@@ -161,17 +169,15 @@ function wp_social_bookmarking_light_the_content($content)
     }
 
     $options = wp_social_bookmarking_light_options();
-    $out = wp_social_bookmarking_light_output( $options['services'], get_permalink(), get_the_title() );
-    if( $out == '' ){
+    $out = wp_social_bookmarking_light_output($options['services'], get_permalink(), get_the_title());
+    if ($out == '') {
         return $content;
     }
-    if( $options['position'] == 'top' ){
+    if ($options['position'] === 'top') {
         return "{$out}{$content}";
-    }
-    else if( $options['position'] == 'bottom' ){
+    } elseif ($options['position'] === 'bottom') {
         return "{$content}{$out}";
-    }
-    else if( $options['position'] == 'both'){
+    } elseif ($options['position'] === 'both') {
         return "{$out}{$content}{$out}";
     }
     return $content;
@@ -182,9 +188,8 @@ function wp_social_bookmarking_light_the_content($content)
  */
 function wp_social_bookmarking_light_wp_footer()
 {
-?>
-<!-- BEGIN: WP Social Bookmarking Light -->
-<?php
+    $out = "<!-- BEGIN: WP Social Bookmarking Light -->\n";
+
     // load options
     $options = wp_social_bookmarking_light_options();
     $services = explode(",", $options['services']);
@@ -194,17 +199,22 @@ function wp_social_bookmarking_light_wp_footer()
      */
     // twitter
     if (in_array('twitter', $services)) {
-        echo "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>\n";
+        $out .= "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>\n";
     }
+
     // evernote
     if (in_array('evernote', $services)) {
-        echo '<script type="text/javascript" src="http://static.evernote.com/noteit.js"></script>'."\n";
+        $out .= '<script type="text/javascript" src="http://static.evernote.com/noteit.js"></script>' . "\n";
     }
+
     // Google +1
     if (in_array('google_plus_one', $services)) {
         $lang = $options['google_plus_one']['lang'];
-        echo '<script src="https://apis.google.com/js/platform.js" async defer>{lang: "'.$lang.'"}</script>'."\n";
+        $out .= '<script src="https://apis.google.com/js/platform.js" async defer>'
+            . '{lang: "' . $lang . '"}'
+            . "</script>\n";
     }
+
     // pinterest
     if (in_array('pinterest', $services)) {
         if ($options['pinterest']['type'] === 'all') {
@@ -213,23 +223,22 @@ function wp_social_bookmarking_light_wp_footer()
             $data_pin_hover = 'data-pin-hover="true"';
             $shape = $options['pinterest']['shape'];
             $data_pin_shape = $shape === 'round' ? 'data-pin-shape="round"' : '';
-            $data_pin_color = 'data-pin-color="'.$options['pinterest']['color'];
-            $data_pin_lang = 'data-pin-lang="'.$options['pinterest']['lang'];
+            $data_pin_color = 'data-pin-color="' . $options['pinterest']['color'];
+            $data_pin_lang = 'data-pin-lang="' . $options['pinterest']['lang'];
             $data_pin_height = '';
             if ($options['pinterest']['size'] === 'large') {
                 $data_pin_height = $shape === 'round' ? 'data-pin-height="32"' : 'data-pin-height="28"';
             }
         }
-        echo '<script type="text/javascript" async defer  '
-            .$data_pin_shape.' '
-            .$data_pin_color.' '
-            .$data_pin_lang.' '
-            .$data_pin_height.' '
-            .$data_pin_hover.' '
-            .'src="//assets.pinterest.com/js/pinit.js"></script>';
+        $out .= '<script type="text/javascript" async defer  '
+            . $data_pin_shape . ' '
+            . $data_pin_color . ' '
+            . $data_pin_lang . ' '
+            . $data_pin_height . ' '
+            . $data_pin_hover . ' '
+            . 'src="//assets.pinterest.com/js/pinit.js"></script>' . "\n";
     }
 
-    ?>
-<!-- END: WP Social Bookmarking Light -->
-<?php
+    $out .= "<!-- END: WP Social Bookmarking Light -->\n";
+    echo $out;
 }
